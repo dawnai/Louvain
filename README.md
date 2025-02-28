b25d2bdf9083c0c7c3ee61726b474923d44c1a64
dawnai <dawnai@163.com>
2025-02-28 17:23:22 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Louvain
-louvain算法实验
+<img src="https://dawnai.cloud/img/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20230206145629.ae57fb1d.jpg" style="zoom:50%;" />
+
+# Louvain 事件聚类 👋
+
+使用之前确保neo4j已经存储了5w1h数据，若没有数据，可用tool/upload_data.py上传数据
+
+### 使用：
+
+修改 main.py文件，配置对应的neo4j数据库信息，和ollama配置信息
+
+```
+python main.py
+```
+
---
+++ main.py
@@ -0,0 +1,54 @@
+"""
+主函数
+1、导出数据：what节点、what节点属性name why how、time节点、who节点、where节点
+2、分别计算语义相似度权重和关系权重
+3、构建louvain需要的图
+4、进行社区发现
+5、结果分析
+6、写回neo4j数据库
+"""
+import logging
+from tool.Neo4jLouvainProcessor import Neo4jLouvainProcessor
+# 配置日志
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    # neo4j数据库配置信息
+    config = {
+        "uri": "bolt://172.20.31.130:7687",#neo4j地址
+        "user": "neo4j",
+        "password": "neo4j@openspg",
+        "db_name": "dawnjiang5w1h",#neo4j数据库
+        "semantic_threshold": 0.8,  #语义相似度阈值
+        "ollama_uri":'http://172.20.31.130:11434',#ollama地址
+        "model_name":'bge-m3'#模型
+    }
+
+    processor = Neo4jLouvainProcessor(**config)
+
+    try:
+        # Step 1: 数据导出
+        processor.export_data()
+        
+        # Step 2: 权重计算
+        processor.calculate_semantic_weights()
+        processor.calculate_relation_weights()
+        
+        # Step 3: 构建图
+        processor.build_graph()
+        
+        # Step 4: 社区发现
+        processor.detect_communities()
+        
+        # Step 5: 结果分析
+        processor.analyze_results()
+        
+        # Step 6: 写回结果
+        processor.write_results()
+
+    except Exception as e:
+        logger.error(f"流程执行失败: {str(e)}")
+    finally:
+        processor.close()
+        logger.info("处理完成，连接已关闭")
